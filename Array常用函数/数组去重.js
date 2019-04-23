a226bbb70f42e7ab42538db5fd885e57fe99544b
hyang <1348261663@qq.com>
2019-04-23 17:33:51 +0800

    Update 数组去重.js

--- Array常用函数/数组去重.js
+++ Array常用函数/数组去重.js
@@ -17,3 +17,16 @@ function unique1(arr){
  }
  return res;
 }
+
+//2.先将原数组排序，在与相邻的进行比较，如果不同则存入新数组
+
+function unique2(arr){
+ var arr2 = arr.sort();
+ var res = [arr2[0]];
+ for(var i=1; i<arr2.length; i++){
+  if(arr2[i] !== res[res.length-1]){
+   res.push(arr2[i]);
+  }
+ } 
+ return res;
+}
