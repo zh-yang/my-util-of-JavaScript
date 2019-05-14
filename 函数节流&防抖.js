c282f0160c9bdcfe031ebc4a24c590d57707f380
hyang <1348261663@qq.com>
2019-05-14 22:12:01 +0800

    Update 函数节流&防抖.js

--- 函数节流&防抖.js
+++ 函数节流&防抖.js
@@ -1,1 +1,12 @@
 
+//节流
+function throttle(fn, dealy){
+  let canUse = true;
+  return function(){
+    if(canUse){
+      fn.apply(this, arguments);
+      canUse = false;
+      setTimeout(()=>canUse = true, dealy);
+    }
+  }
+}
